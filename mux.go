80e5103ca61f739756a1be21b2fd81457b9040d8
yohta.togasaki <y.togasaki@icloud.com>
2024-08-17 21:39:39 +0900

    chapter16完了

--- mux.go
+++ mux.go
@@ -7,7 +7,6 @@ func NewMux() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		// 静的解析のエラーを回避するため明示的に戻り値を捨てている
 		w.Write([]byte(`{"status": "ok"}`))
 	})
 	return mux
